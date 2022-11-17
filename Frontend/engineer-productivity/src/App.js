49e819d2fbc1b463565d84b32b51dde02fcf203c
Aj Pakulyte <pakulyta@tcd.ie>
2022-11-17 13:59:58 +0000

    Created timeline for tracking issue duration.

--- Frontend/engineer-productivity/src/App.js
+++ Frontend/engineer-productivity/src/App.js
@@ -3,11 +3,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LineCharts from './components/LineCharts';
+import Timeline from './components/Timeline';
 
 function App() {
   return (
     <div className="App">
      <LineCharts />
+     <Timeline/>
     </div>
   );
 }
