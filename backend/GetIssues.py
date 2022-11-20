b2131fc8fcef6e3bcea37f115bb2b993234236f7
bondarus <bondarus@tcd.ie>
2022-11-20 19:48:05 +0000

    Defined a function for issues and created a blank test file

--- backend/GetIssues.py
+++ backend/GetIssues.py
@@ -9,16 +9,21 @@ print("Status code: ", response.status_code)
 response_dict = response.json()
 print('Open issues: ', response_dict['open_issues_count'],'\n')
 
-response = requests.get(issue_times)
-response_dict = response.json()
+def getRecentIssues():
+    response = requests.get(issue_times)
+    response_dict = response.json()
 
-for i in range(len(response_dict)):
-    issue = response_dict[i]
-    print('Issue ID: ', issue['number'])
-    dateCreated = issue['created_at'].replace('Z', 'T').split('T')
-    dateClosed = issue['closed_at'].replace('Z', 'T').split('T')
-    issue_id = issue['number']
-    dateCreated[2] = issue_id
-    dateClosed[2] = issue_id
-    print('Date Created: ', dateCreated)
-    print('Date Closed: ', dateClosed)
+    for i in range(len(response_dict)):
+        issue = response_dict[i]
+        print('Issue ID: ', issue['number'])
+        dateCreated = issue['created_at'].replace('Z', 'T').split('T')
+        dateClosed = issue['closed_at'].replace('Z', 'T').split('T')
+        issue_id = issue['number']
+        dateCreated[2] = issue_id
+        dateClosed[2] = issue_id
+        print('Date Created: ', dateCreated)
+        print('Date Closed: ', dateClosed)
+
+    return dateCreated
+
+getRecentIssues()
