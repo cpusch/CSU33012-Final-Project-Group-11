0e0af0d81778b23c1a42bb0e6869ad1f7ce9705b
cpusch <conpusch@yahoo.com>
2022-11-21 17:49:58 +0000

    uncommented unused tests

--- backend/app/test.py
+++ backend/app/test.py
@@ -3,8 +3,8 @@ from main import *
 def test_workflow():
     assert 1 == 1
 
-def test_commits():
-    return getCommitsOverTime("AUTOMATIC1111/stable-diffusion-webui")
+# def test_commits():
+#     return getCommitsOverTime("AUTOMATIC1111/stable-diffusion-webui")
 
-print(test_commits())
+# print(test_commits())
 
