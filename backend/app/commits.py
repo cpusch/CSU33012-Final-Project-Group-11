89dbc738f7bd996e77a64ea24e3d35ff0ae3f8b6
Abigail Amelia Amethyst <abigail.amethyst@protonmail.com>
2022-11-23 16:53:06 +0000

    Added option for years and months for commits function

--- backend/app/commits.py
+++ backend/app/commits.py
@@ -1,56 +1,30 @@
-import json
 import requests
+from dotenv import dotenv_values
+SECRETS = dotenv_values("./../.env")
+"""
+getCommitsOverTime
+Uses the GitHub REST API to retrieve the number of commits made over time on the master branch
+Author: Abigail Amethyst
 
-# getCommitsOverTime
-# Uses the GitHub REST API to retrieve the number of commits made over time on the master branch
-# Author: Abigail Amethyst
-# project - name of requested project, stores as a String (formatted as 'owner/repoName')
-# does not return anything, but creates a .json file with 2 lists
-# the first list is the timespan of the repo in months, the second is the amount of commits made per month
-# the index of both arrays correspond with each other
-def getCommitsOverTime(project):
-    # initialising months and commits list for storage of data
-    months = []
-    commits = []
+project - name of requested project, stores as a String (formatted as 'owner/repoName')
+isYears - Boolean value determining whether the commit dict will be in years or months
+
+Returns a dict with the chosen timespan and the number of commits for each year/month
+"""
+def getCommitsOverTime(project, isYears):
     # getting information about specified project
     url = f'https://api.github.com/repos/{project}'
-    resp = requests.get(url)
-    respDict = resp.json()
-    # getting creation date
-    creationDate = respDict['created_at']
-    creationDate = creationDate[:7]
-    # getting date of latest update
-    latestUpdate = respDict['updated_at']
-    latestUpdate = latestUpdate[:7]
-    # storing both dates as integer lists
-    year = int(creationDate[:4])
-    month = int(creationDate[5:7])
-    currentDate = [ year, month ]
-    year = int(latestUpdate[:4])
-    month = int(latestUpdate[5:7])
-    latestDate = [ year, month ]
-
-    # generating the months and commit lists from month of creation to month of latest update for storing data
-    while currentDate != latestDate:
-        if currentDate[1] < 10:
-            months.append(f'{currentDate[0]}-0{currentDate[1]}')
-        else:
-            months.append(f'{currentDate[0]}-{currentDate[1]}')
-        commits.append(0)
-        currentDate[1] += 1
-        # reset month count and increment year count if necessary
-        if currentDate[1] > 12:
-            currentDate[1] = 1
-            currentDate[0] += 1
-    if currentDate[1] < 10:
-        months.append(f'{currentDate[0]}-0{currentDate[1]}')
+    resp = requests.get(url, headers={"authorization": SECRETS['GITHUB_TOKEN']})
+    if isYears:
+        timeCommits = initYears(resp)
     else:
-        months.append(f'{currentDate[0]}-{currentDate[1]}')
-    commits.append(0)
+        timeCommits = initMonths(resp)
+    timespan = timeCommits[0]
+    commits = timeCommits[1]
 
     # getting list of branches for given repo
     url = f'https://api.github.com/repos/{project}/branches?per_page=100'
-    resp = requests.get(url)
+    resp = requests.get(url, headers={"authorization": SECRETS['GITHUB_TOKEN']})
     respDicts = resp.json()
     # look for master branch
     for i in range(0, len(respDicts)):
@@ -59,8 +33,8 @@ def getCommitsOverTime(project):
         if name == 'master':
             # get list of 100 latest commits from master branch
             sha = respDict['commit']['sha']
-            url = f'https://api.github.com/repos/{project}/branches?per_page=100&sha={sha}'
-            resp = requests.get(url)
+            url = f'https://api.github.com/repos/{project}/commits?per_page=100&sha={sha}'
+            resp = requests.get(url, headers={"authorization": SECRETS['GITHUB_TOKEN']})
             commitDicts = resp.json()
             # parse list and increment commits by their respective months
             while len(commitDicts) != 1:
@@ -68,16 +42,109 @@ def getCommitsOverTime(project):
                 while j <= len(commitDicts)-1:
                     commit = commitDicts[j]
                     date = commit['commit']['author']['date']
-                    date = date[:7]
-                    commits[months.index(date)] += 1
+                    if isYears:
+                        date = date[:4]
+                    else:
+                        date = date[:7]
+                    commits[timespan.index(date)] += 1
                     j += 1
                 # get sha of last commit in list to get next 100 commits
                 commit = commitDicts[j]
                 sha = commit['sha']
                 url = f'https://api.github.com/repos/{project}/branches?per_page=100&sha={sha}'
-                resp = requests.get(url)
+                resp = requests.get(url, headers={"authorization": SECRETS['GITHUB_TOKEN']})
                 commitDicts = resp.json()
+
     # generate .json file with dictionary of commits per month
-    jsonDict = dict(zip(months, commits))
-    with open("commits.json", "w") as outfile:
-        json.dump(jsonDict, outfile)
+    jsonDict = dict(zip(timespan, commits))
+    return jsonDict
+
+
+"""
+initMonths
+Initialises 2 lists with months from the creation date of a repo to the latest update of a repo
+Author: Abigail Amethyst
+
+resp - response gotten from the GitHub API call
+
+Returns a list with 2 lists within
+The first list is a list of the months from the creation date to the latest update
+The second list is a list of all the commits for the timespan initialised to 0
+The index of the first list corresponds to the amount of commits in the second list at the same index
+"""
+def initMonths(resp):
+    timeList = []
+    commitList = []
+    respDict = resp.json()
+    # getting creation date
+    creationDate = respDict['created_at']
+    creationDate = creationDate[:7]
+    # getting date of the latest update
+    latestUpdate = respDict['updated_at']
+    latestUpdate = latestUpdate[:7]
+    # storing both dates as integer lists
+    year = int(creationDate[:4])
+    month = int(creationDate[5:7])
+    currentDate = [year, month]
+    year = int(latestUpdate[:4])
+    month = int(latestUpdate[5:7])
+    latestDate = [year, month]
+
+    # generating the months and commit lists from month of creation to month of the latest update for storing data
+    while currentDate != latestDate:
+        if currentDate[1] < 10:
+            timeList.append(f'{currentDate[0]}-0{currentDate[1]}')
+        else:
+            timeList.append(f'{currentDate[0]}-{currentDate[1]}')
+        commitList.append(0)
+        currentDate[1] += 1
+        # reset month count and increment year count if necessary
+        if currentDate[1] > 12:
+            currentDate[1] = 1
+            currentDate[0] += 1
+    if currentDate[1] < 10:
+        timeList.append(f'{currentDate[0]}-0{currentDate[1]}')
+    else:
+        timeList.append(f'{currentDate[0]}-{currentDate[1]}')
+    commitList.append(0)
+
+    output = [ timeList, commitList ]
+    return output
+
+
+"""
+initYears
+Initialises 2 lists with years from the creation date of a repo to the latest update of a repo
+Author: Abigail Amethyst
+
+resp - response gotten from the GitHub API call
+
+Returns a list with 2 lists within
+The first list is a list of the years from the creation date to the latest update
+The second list is a list of all the commits for the timespan initialised to 0
+The index of the first list corresponds to the amount of commits in the second list at the same index
+"""
+def initYears(resp):
+    timeList = []
+    commitList = []
+    respDict = resp.json()
+    # getting creation date
+    creationDate = respDict['created_at']
+    creationDate = creationDate[:4]
+    # getting date of the latest update
+    latestUpdate = respDict['updated_at']
+    latestUpdate = latestUpdate[:4]
+    # storing both dates as integer lists
+    currentYear = creationDate
+    latestDate = latestUpdate
+
+    # generating the months and commit lists from month of creation to month of the latest update for storing data
+    while currentYear != latestDate:
+        timeList.append(currentYear)
+        commitList.append(0)
+        currentYear += 1
+    timeList.append(currentYear)
+    commitList.append(0)
+
+    output = [timeList, commitList]
+    return output
