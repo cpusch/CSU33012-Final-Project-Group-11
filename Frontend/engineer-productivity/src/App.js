4c9bbd4609cc1e3459b681a12f134487951813a3
mfatima <mfatima@tcd.ie>
2022-11-23 14:39:31 +0000

    dropdown added to view data in diff representation

--- Frontend/engineer-productivity/src/App.js
+++ Frontend/engineer-productivity/src/App.js
@@ -4,10 +4,17 @@ import './App.css';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import LineCharts from './components/LineCharts';
 import Timeline from './components/Timeline';
+import Dropdown from './components/Dropdown';
+
 
 function App() {
+  const options = [
+    {value: "Monthly", label: "Monthly"},
+    {value: "Yearly", label: "Yearly"},
+];
   return (
     <div className="App">
+    <Dropdown placeHolder="Select..." options={options}/>
      <LineCharts />
      <Timeline/>
     </div>
--- Frontend/engineer-productivity/src/components/LineCharts.js
+++ Frontend/engineer-productivity/src/components/LineCharts.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 import Chart from 'react-google-charts'
 export const LineData = [
   [
-    { type: "date", id: "Date" },
+    { type: "string", id: "Date" },
     { type: "number", id: "Commits" },
   ],
-  [new Date(2022, 0, 13), 0],
-  [new Date(2022, 1, 17), 12],
-  [new Date(2022, 2, 22), 6],
-  [new Date(2022, 3, 29), 9],
-  [new Date(2022, 4, 1), 5],
-  [new Date(2022, 4, 3), 5],
+  ["January", 0],
+  ["February", 12],
+  ["March", 6],
+  ["April", 9],
+  ["May", 5],
+  ["June", 5],
 ]
 const LineChartOptions = {
   hAxis: {
