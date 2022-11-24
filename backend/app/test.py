6abcf1ca8cf497c14031338e07798fff5f3b4e2f
DanielMadaghjian <danielmadaghjian@gmail.com>
2022-11-24 09:47:11 +0000

    wrote simple tests in test.py, for the get_commits and get_issues functions

--- backend/app/test.py
+++ backend/app/test.py
@@ -1,10 +1,25 @@
 from main import *
+import requests
 
 def test_workflow():
     assert 1 == 1
 
-# def test_commits():
-#     return getCommitsOverTime("AUTOMATIC1111/stable-diffusion-webui")
+#tests that the api call receives a status code of 200
+def test_status_code():
+    url = f'https://api.github.com/repos/AUTOMATIC1111/stable-diffusion-webui'
+    response = requests.get(url)
+    assert response.status_code == 200, print(
+        "Got wrong status code, expected is: {}, actual is  {}".format("200", response.status_code))
+    
+#tests our get_commits function, checking whether the dictionary returned is not null
+def test_commits():
+    jsonDict = get_commits()
+    assert len(jsonDict) != 0
+
+#tests our get_issues function, checking whether the dictionary returned is not null 
+def test_issues():
+    jsonDict = get_issues()
+    assert len(jsonDict) != 0
 
 # print(test_commits())
 
