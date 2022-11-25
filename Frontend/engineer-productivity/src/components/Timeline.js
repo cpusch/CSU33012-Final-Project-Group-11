52dfe0dbc99a6c1131c5c09c01f8da1d5b921621
cpusch <conpusch@yahoo.com>
2022-11-25 16:35:32 +0000

    updated packages

--- Frontend/engineer-productivity/src/components/Timeline.js
+++ Frontend/engineer-productivity/src/components/Timeline.js
@@ -3,29 +3,31 @@ import Chart from 'react-google-charts'
 
 // sample data
 export const data = [
-  [
-    { type: "string", id: "Issue Name" },
-    { type: "date", id: "Issue Made" },
-    { type: "date", id: "Issue Closed" },
-  ],
-  ["Create CI/CD Workflow", new Date(2022, 0, 13), new Date(2022, 0, 15)],
-  ["Fix bug with input", new Date(2022, 1, 4), new Date(2022, 1, 28)],
-  ["Push to production", new Date(2022, 2, 1), new Date(2022, 3, 7)],
+    [
+        { type: "string", id: "Issue Name" },
+        { type: "date", id: "Issue Made" },
+        { type: "date", id: "Issue Closed" },
+    ],
+    ["Create CI/CD Workflow", new Date(2022, 0, 13), new Date(2022, 0, 15)],
+    ["Fix bug with input", new Date(2022, 1, 4), new Date(2022, 1, 28)],
+    ["Push to production", new Date(2022, 2, 1), new Date(2022, 3, 7)],
 ];
+
+
 
 class Timeline extends Component {
     render() {
-      return (
-        <div className="container mt-5">
-          <Chart
-            width={'100%'}
-            height={'400px'}
-            chartType="Timeline"
-            loader={<div>Loading Timeline</div>}
-            data={data}
-          />
-        </div>
-      )
+        return (
+            <div className="container mt-5">
+                <Chart
+                    width={'100%'}
+                    height={'400px'}
+                    chartType="Timeline"
+                    loader={<div>Loading Timeline</div>}
+                    data={data}
+                />
+            </div>
+        )
     }
-  }
-  export default Timeline
+}
+export default Timeline
