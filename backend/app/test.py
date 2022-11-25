237c1d82a77be3e911498875248b1476b239bc73
cpusch <conpusch@yahoo.com>
2022-11-25 22:14:09 +0000

    moved tests from test_getissues to test.py

--- backend/app/test.py
+++ backend/app/test.py
@@ -1,10 +1,14 @@
 from main import *
+import GetIssues
 
 def test_workflow():
     assert 1 == 1
 
-# def test_commits():
-#     return getCommitsOverTime("AUTOMATIC1111/stable-diffusion-webui")
+def test_status():
+    result = GetIssues.get_url_status()
+    assert result == 200
 
-# print(test_commits())
+def test_issues_length():
+    result = GetIssues.getRecentIssues()
+    assert len(result) == 100
 
