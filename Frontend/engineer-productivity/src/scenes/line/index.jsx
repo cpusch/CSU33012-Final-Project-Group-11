bf34d4e497f32b11423153ab4de0cb7790256ede
DanielMadaghjian <danielmadaghjian@gmail.com>
2022-11-30 18:23:49 +0000

    fixed bugs of graphs not showing

--- Frontend/engineer-productivity/src/scenes/line/index.jsx
+++ Frontend/engineer-productivity/src/scenes/line/index.jsx
@@ -1,12 +1,13 @@
 import { Box } from "@mui/material";
 import Header from "../../components/Header";
-
+import LineChart from "../../components/LineCharts";
 
 const Line = () => {
   return (
     <Box m="20px">
       <Header title="Pull" subtitle="" />
       <Box height="75vh">
+        <LineChart />
       </Box>
     </Box>
   );
