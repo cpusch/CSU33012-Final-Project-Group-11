aa34d107705cfaf4739d71d1cb44840aac7765cd
mfatima <mfatima@tcd.ie>
2022-11-30 17:21:20 +0000

    line chart with backend data

--- Frontend/engineer-productivity/src/components/LineCharts.js
+++ Frontend/engineer-productivity/src/components/LineCharts.js
@@ -1,32 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Chart from 'react-google-charts'
 import axios from 'axios'
-import { data } from './Timeline';
-
-const m = 4;
+// import { data } from './Timeline';
+//let header = ["Commits", 0];
+const m = 0;
 const  n = 2;
-let arr = new Array(m); // create an empty array of length n
+let dataArr = new Array(m); // create an empty array of length n
 //let innerArray = new Array(0);
 for (var i = 0; i < m; i++) {
-  arr[i] = new Array(n); // make each element an array
+  dataArr[i] = new Array(n); // make each element an array
 }
 
+/*axios.get("http://127.0.0.1:8000/commits")
+  .then(function (response) {
+      console.log((response.data));
+      let array = (response.data);
+      console.log((array));
+      dataArr.push(array[0]);
+      dataArr.push(array[1]);
+      dataArr.push(array[2]);
+      dataArr.push(array[3]);
+      console.log(dataArr);
+  })*/
 
-  axios.get("http://127.0.0.1:8000/commits")
-    .then(function (response) {
-        console.log((response.data));
-        arr = (response.data);
-    })
+  function LineCharts() {
+    const [dataHook, setDataHook] = useState([]);
+    const getData = async () => {
+        await axios.get("http://127.0.0.1:8000/commits")
+            .then((response) => (response.data))
+            .then((data) => {
+                setDataHook(data)
+            })
+    }
+    getData()
+    console.log(dataHook)
 
-export const LineData  = [
+  
+const LineData  = [
   [
     { type: "string", id: "Date" },
     { type: "number", id: "Commits" },
   ],
-  arr[0],
-  arr[1],
-  arr[2],
-  arr[3],
+  dataHook[0],
+  dataHook[1],
+  dataHook[2],
+  dataHook[3],
 ]
 const LineChartOptions = {
   hAxis: {
@@ -40,15 +58,16 @@ const LineChartOptions = {
   
   },
 }
-class LineCharts extends Component {
+/*class LineCharts extends Component {
 
   constructor(props){
     super(props);
     this.state ={
         chartData:props.chartData
     }
-}
-  render() {
+}*/
+  //render() {
+     //console.log(this.state.chartData);
     return (
       <div className="container mt-5">
         <h4>Line Chart for Commits over a period of time</h4>
@@ -64,6 +83,6 @@ class LineCharts extends Component {
         />
       </div>
     )
-  }
+   //}
 }
 export default LineCharts
