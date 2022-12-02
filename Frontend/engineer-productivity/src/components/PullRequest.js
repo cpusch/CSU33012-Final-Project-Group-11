4f0d613c5bb9af66726984fdf47d4989cf416a49
cpusch <conpusch@yahoo.com>
2022-12-02 12:28:45 +0000

    updated pulllrequests to useEffect()

--- Frontend/engineer-productivity/src/components/PullRequest.js
+++ Frontend/engineer-productivity/src/components/PullRequest.js
@@ -1,57 +1,55 @@
-import React, { Component, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Chart from 'react-google-charts'
 import axios from 'axios'
 
 function PullRequest() {
-  const [dataHook, setDataHook] = useState([]);
-  const getData = async () => {
-      await axios.get("http://127.0.0.1:8000/pulls")
-          .then((response) => (response.data))
-          .then((data) => {
-              setDataHook(data)
-          })
-  }
-  getData()
-  console.log(dataHook)
- const BarData = [
-  [
-    { type: "string", id: "Date" },
-    { type: "number", id: "Pulls" },
-  ],
-  dataHook[0],
-  dataHook[1],
-  dataHook[2],
-  dataHook[3],
-  dataHook[4],
-]
-const BarChartOptions = {
-  hAxis: {
-    title: 'Commits',
-  },
-  vAxis: {
-    title: 'Time',
-  },
-  series: {
-    0: {color: '#9d66cd'},
-  },
-}
+    const [dataHook, setInfo] = useState([]);
+    useEffect(() => {
+        const getData = async () => {
+            const res = await axios('http://127.0.0.1:8000/pulls');
+            // console.log(res.data);
+            setInfo(res.data);
+        };
 
+        getData();
+    }, []);
+    const BarData = [
+        [
+            { type: "string", id: "Date" },
+            { type: "number", id: "Pulls" },
+        ],
+        dataHook[0],
+        dataHook[1],
+        dataHook[2],
+        dataHook[3],
+        dataHook[4],
+    ]
+    const BarChartOptions = {
+        hAxis: {
+            title: 'Commits',
+        },
+        vAxis: {
+            title: 'Time',
+        },
+        series: {
+            0: { color: '#9d66cd' },
+        },
+    }
 
-  
     return (
-      <div className="container mt-5">
-        <Chart
-          width={'700px'}
-          height={'410px'}
-          chartType="Bar"
-          loader={<div>Loading Chart</div>}
-          //data={this.state.chartData}
-          data={BarData}
-          options={BarChartOptions}
-          rootProps={{ 'data-testid': '2' }}
-        />
-      </div>
+        <div className="container mt-5">
+            <Chart
+                width={'700px'}
+                height={'410px'}
+                chartType="Bar"
+                loader={<div>Loading Chart</div>}
+                //data={this.state.chartData}
+                data={BarData}
+                options={BarChartOptions}
+                rootProps={{ 'data-testid': '2' }}
+            />
+        </div>
     )
-  }
+}
 
 export default PullRequest
